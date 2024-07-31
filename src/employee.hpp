630e8b43d92fa35af841224f1eb601e86b35a8c9
Klaudia Szczepańska <87570436+KSzczepa@users.noreply.github.com>
2024-07-31 18:19:11 +0200

    empleyee header file

---
+++ src/employee.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include <string>
+
+class Employee {
+public:
+    Employee(std::string name,
+            std::string lastName,
+            std::string address,
+            int number,
+            std::string pesel,
+            Gender gender
+            )
+};
