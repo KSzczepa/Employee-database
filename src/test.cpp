972e9ca177010f980d0360ca0bac9ac9cf5f470e
Klaudia Szczepańska <87570436+KSzczepa@users.noreply.github.com>
2024-07-31 18:19:20 +0200

    Test update

--- src/test.cpp
+++ src/test.cpp
@@ -1,4 +1,6 @@
 #include <gtest/gtest.h>
+#include "employee.hpp"
+#include "database.hpp"
 
 // Demonstrate some basic assertions.
 TEST(HelloTest, BasicAssertions) {
@@ -6,4 +8,11 @@ TEST(HelloTest, BasicAssertions) {
   EXPECT_STRNE("hello", "world");
   // Expect equality.
   EXPECT_EQ(7 * 6, 42);
+}
+
+TEST(ChechStructure, CanAddEmployee) {
+    Empl adam {"Adam", "Kowalsko", "ul. Dluga 15, 45-030 Opole", 123497, "11223300558", Gender::Male}
+
+    Database db;
+    db.add(adam);
 }
