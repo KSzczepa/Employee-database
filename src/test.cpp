1fe83f00e16456486aec0346359acbad8d3b4417
Klaudia Szczepańska <87570436+KSzczepa@users.noreply.github.com>
2024-08-04 14:17:42 +0200

    Test update - add empl

--- src/test.cpp
+++ src/test.cpp
@@ -2,17 +2,11 @@
 #include "employee.hpp"
 #include "database.hpp"
 
-// Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
-}
 
-TEST(ChechStructure, CanAddEmployee) {
-    Empl adam {"Adam", "Kowalsko", "ul. Dluga 15, 45-030 Opole", 123497, "11223300558", Gender::Male}
+TEST(CheckStructure, CanAddEmployee) {
+    Employee adam {"Adam", "Kowalsko", "ul. Dluga 15, 45-030 Opole", 123497, "11223300558", Gender::Male};
 
     Database db;
-    db.add(adam);
+    EXPECT_TRUE(db.add(adam));
+    EXPECT_FALSE(db.add(adam));
 }
