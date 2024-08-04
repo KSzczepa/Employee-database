b3e8b2d488c6765120b18e757fc4e434074670bf
Klaudia Szczepańska <87570436+KSzczepa@users.noreply.github.com>
2024-08-04 15:13:27 +0200

    New database tests

--- src/test.cpp
+++ src/test.cpp
@@ -7,13 +7,6 @@ struct DatabaseTest : ::testing::Test
 	Database db;
 };
 
-TEST(DatabaseTest, CanAddEmployee) {
-    Employee adam {"Adam", "Kowalsko", "ul. Dluga 15, 45-030 Opole", 123497, "11223300558", Gender::Male};
-
-    EXPECT_TRUE(db.add(adam));
-    EXPECT_FALSE(db.add(adam));
-}
-
 TEST(DatabaseTest, DisplayEmptyDb) {
 	auto content = db.show();
 	auto expected = "";
@@ -22,7 +15,9 @@ TEST(DatabaseTest, DisplayEmptyDb) {
 
 TEST(DatabaseTest, DisplayNonEmptyDb) {
 	Employee adam {"Adam", "Kowalsko", "ul. Dluga 15, 45-030 Opole", 123497, "11223300558", Gender::Male};
-	EXPECT_TRUE(db.add(adam));
+	db.add(adam);
 
-    db.display();
+    auto content = db.show();
+	auto expected = "Adam Kowalsko; ul. Dluga 15, 45-030 Opole; 123497; 11223300558; Male";
+	EXPECT_EQ(content, expected);
 }
